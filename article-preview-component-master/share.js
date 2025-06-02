9f2e294fe5430d68bd85ac0372b72d7c7b891844
niloofar <sn.mousavi77@gmail.com>
2025-06-02 17:02:01 +0330

    changes added

--- article-preview-component-master/share.js
+++ article-preview-component-master/share.js
@@ -1,1 +1,7 @@
-let share=document.querySelector("#container")
+document.querySelector('.share-icon').addEventListener('mouseenter', function() {
+    document.querySelector('.share-options').style.display = 'block';
+});
+
+document.querySelector('.share-icon-container').addEventListener('mouseleave', function() {
+    document.querySelector('.share-options').style.display = 'none';
+});
